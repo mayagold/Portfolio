81a4991c35ff90a5307a006d7c66bbba7efaa733
mayagold <maya.gold.1991@gmail.com>
2017-09-22 14:34:43 -0700

    styled for mobile

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 console.log("hi");
 
+// jQuery NavBar Function
 
-$( ()=>{
-  var $navbar = $("#nav-header"),
-      y_pos   = $navbar.offset().top,
-      height  = $navbar.height();
+// $( ()=>{
+//   var $navbar = $("#nav-header"),
+//       y_pos   = $navbar.offset().top,
+//       height  = $navbar.height();
+//   $(document).scroll(function(){
+//     var scrollTop = $(this).scrollTop();
+//
+//     if (scrollTop > y_pos + height) {
+//       $navbar.addClass("navbar-fixed").animate({
+//         top: 0
+//       });
+//     } else if (scrollTop <= y_pos) {
+//       $navbar.removeClass("navbar-fixed").clearQueue().animate({
+//         top: "-48px"
+//       }, .1);
+//     }
+//   });
+// })
 
-  $(document).scroll(function(){
-    var scrollTop = $(this).scrollTop();
-
-    if (scrollTop > y_pos + height) {
-      $navbar.addClass("navbar-fixed").animate({
-        top: 0
-      });
-    } else if (scrollTop <= y_pos) {
-      $navbar.removeClass("navbar-fixed").clearQueue().animate({
-        top: "-48px"
-      }, .1);
-    }
-  });
-
-})
+//
