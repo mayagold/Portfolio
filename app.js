927821b0273b5aed9ed4dfd3b0fde49849f05ac0
mayagold <maya.gold.1991@gmail.com>
2017-09-25 19:46:34 -0700

    cleaning

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ console.log("hi");
 
 $(window).scroll(function(){
     $("#headshot").css("opacity", 1 - $(window).scrollTop() / 300);
+    $("#development").css("opacity", 1 - $(window).scrollTop() / 100);
+    $("#design").css("opacity", 1 - $(window).scrollTop() / 100);
     $(".name").css("opacity", 1 - $(window).scrollTop() / 350);
     $('h2').css("opacity", 1 - $(window).scrollTop() / 550);
-  });
 
+
+
+  });
 $( ()=>{
 
 
