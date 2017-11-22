829ff807a1dd3e2912632eb079000ec7d799cb77
mayagold <maya.gold.1991@gmail.com>
2017-11-22 14:04:02 -0800

    added CSS animations and updated styling for mobile

--- app.js
+++ app.js
@@ -2,12 +2,19 @@
 
 $(document).ready( ()=> {
   const headshot = $('.image-headshot');
+  const welcome = $('.header-box');
   const aboutPosition = $('.about-me').offset().top/4;
+
   $(window).scroll(function(){
     var scrollTop = $(this).scrollTop();
+    console.log(scrollTop);
     headshot.css({ 'top': (aboutPosition - scrollTop)});
+
     if (scrollTop > aboutPosition) {
-      headshot.css({'top': 0})
+      headshot.css({'top': 0});
     }
+
+
+
   });
 });
